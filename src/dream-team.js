42310e200c20a3e098e6f3157892e8e5f859a94f
Bulrock <maksim.volozhinskij@gmail.com>
2022-10-11 21:50:40 +0300

    feat: implement dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let tempArr = [];
+  const removeSpaces = (str) => str.replace(/\s+/g, '')
+  const isAlpha = /[A-Za-z]/;
+  if(!Array.isArray(members)){
+    return false
+  }
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] !== 'string'){
+      continue
+    } else{
+      let removedSpacesString = removeSpaces(members[i]);
+      if (isAlpha.test(removedSpacesString[0])){
+        tempArr.push(removedSpacesString.charAt(0).toUpperCase())
+      }else {
+        tempArr = tempArr;
+      }
+    }
+  }
+  tempArr.sort((a,b) => a.charCodeAt(0) - b.charCodeAt(0))
+  return tempArr.join('')
 }
 
 module.exports = {
