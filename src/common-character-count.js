8cbd8049be6e2547af5368989c12d5e9196270bb
Bulrock <maksim.volozhinskij@gmail.com>
2022-10-11 21:54:22 +0300

    feat: implement common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getCommonCharacterCount(s1, s2) {
+  let arrFromS1 = s1.split('');
+  let arrFromS2 = s2.split('');
+  let mySetS1 = new Set(s1);
+  let mySetS2 = new Set(s2);
+  let intersection = new Set([...mySetS1].filter(x => mySetS2.has(x)));
+  let intersectionArr = Array.from(intersection);
+  let result1 = [];
+  let result2 = [];
+  let result = [];
+
+  for(let i = 0; i < intersectionArr.length; i++){
+    let a = 0;
+    for(let j = 0; j < arrFromS1.length; j++){
+      if(intersectionArr[i] === arrFromS1[j]){
+        a +=1
+        result1[i] = a;
+      } else{
+        a = a
+        result1[i] = a;
+      }
+    }
+
+    let b = 0;
+    for(let k = 0; k < arrFromS2.length; k++){
+      if(intersectionArr[i] === arrFromS2[k]){
+        b +=1
+        result2[i] = b;
+      } else{
+        b = b
+        result2[i] = b;
+      }
+    }
+    result[i] = Math.min(result1[i], result2[i])
+  }
+
+  let sum = 0;
+  for (let l = 0; l < result.length; l++){
+    sum = sum + result[l];
+  }
+  return sum
 }
 
 module.exports = {
