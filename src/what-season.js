b16be0e475227e53586df1e6af450dd88375ae56
Bulrock <maksim.volozhinskij@gmail.com>
2022-10-11 21:53:33 +0300

    feat: implement what season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date) {
+  try {
+    let month;
+    if(date === undefined || date === null){
+      return 'Unable to determine the time of year!'
+    } else if (date instanceof Date && !isNaN(date)){
+      month = date.getMonth();
+    } else {
+      throw new Error ('Invalid date!')
+    }
+
+    if(month == 2 || month == 3 || month == 4){
+      return 'spring'
+    } else if(month == 5 || month == 6 || month == 7){
+      return 'summer'
+    } else if(month == 8 || month == 9 || month == 10){
+      return 'autumn'
+    } else {
+      return 'winter'
+    }
+  }
+  catch(e) {
+    throw new Error ('Invalid date!')
+  }
 }
 
 module.exports = {
