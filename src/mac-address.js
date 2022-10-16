81da1c96fc18168a1e15dd2d3c83611cbc44fea7
Bulrock <maksim.volozhinskij@gmail.com>
2022-10-16 20:16:42 +0300

    feat: implement mac-address task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function isMAC48Address(mac) {
+  let macAddressSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+  if (mac.length !== 17) return false;
+
+  for(let i = 2; i < 15; i += 3){
+    if(!mac[i] === '-') return false
+  }
+
+  for(let j = 0; j < mac.length; j++){
+    if(j !== 2 && j !== 5  && j !== 8 && j !== 11 && j !== 14){
+       if(!macAddressSymbols.includes(mac[j])) return false;
+    }
+  }
+
+  return true
 }
+
 module.exports = {
   isMAC48Address
 };
