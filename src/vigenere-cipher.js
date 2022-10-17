70d0d4986babf4c869b08183e344340d13dbd4f2
Bulrock <maksim.volozhinskij@gmail.com>
2022-10-17 23:35:55 +0300

    feat: implement vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect){
+    this.direction = isDirect === undefined ? true : isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!message || !key){
+      throw new Error('Incorrect arguments!');
+    }
+
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let encryptedString = '';
+    let keyIndex = 0;
+
+    for(let i = 0; i < message.length; i++){
+      if(message[i].charCodeAt() > 64 && message[i].charCodeAt() < 91){
+        let messageLetterNumber = message[i].charCodeAt() - 65;
+        let keyLetterNumber = key[keyIndex % key.length].charCodeAt() - 65;
+        let encryptedSymbol = (messageLetterNumber + keyLetterNumber)%26 + 65;
+        encryptedString += String.fromCharCode(encryptedSymbol);
+        keyIndex += 1;
+      } else {
+        encryptedString += message[i];
+      }
+    }
+
+    if(this.direction){
+      return encryptedString;
+    } else {
+      let reversedEncryptedString = '';
+      for(let i = encryptedString.length - 1; i >= 0; i--){
+        reversedEncryptedString += encryptedString[i];
+      }
+      return reversedEncryptedString;
+    }
+  }
+
+  decrypt(message, key) {
+    if(!message || !key){
+      throw new Error('Incorrect arguments!');
+    }
+
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    let decryptedString = '';
+    let keyIndex = 0;
+
+    for(let i = 0; i < message.length; i++){
+      if(message[i].charCodeAt() > 64 && message[i].charCodeAt() < 91){
+        let messageLetterNumber = message[i].charCodeAt() - 65;
+        let keyLetterNumber = key[keyIndex % key.length].charCodeAt() - 65;
+        let decryptedSymbol = ((messageLetterNumber - keyLetterNumber) + 26)%26 + 65;
+        decryptedString += String.fromCharCode(decryptedSymbol);
+        keyIndex += 1;
+      } else {
+        decryptedString += message[i];
+      }
+    }
+
+    if(!this.direction){
+      decryptedString = decryptedString.split('').reverse().join('');
+    }
+    return decryptedString;
   }
 }
 
